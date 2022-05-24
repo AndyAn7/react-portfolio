39782777a46353477a76f8d9578420f5cdd17c6a
Andrew An <andyan7@gmail.com>
2022-05-24 02:02:07 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import './index.scss';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
-<<<<<<< HEAD
 import Layout from './components/Layout';
-=======
->>>>>>> 477ee3230261c78cbc4b3815e27cf1edaa4cfa5b
 
 function App() {
   return (
     <>
       <Routes>
-<<<<<<< HEAD
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<Home />} />
         </Route>
-=======
-        <Route path="/" element={<Home />} />
->>>>>>> 477ee3230261c78cbc4b3815e27cf1edaa4cfa5b
       </Routes>
     </>
   );
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,13 +4,8 @@ const Footer = () => {
     return (   
     <>
   <footer>
-<<<<<<< HEAD
       <a href="mailto: andyan7@gmail.com">andyan7@gmail</a> &#160; || 253.414.5536 || &#160; <a
             href="https://www.linkedin.com/in/andyrooh/"> Andrew An </a>
-=======
-      <a href="mailto: andyan7@gmail.com">Email</a> &nbsp || &nbsp 253.414.5536 &nbsp || &nbsp <a
-            href="https://www.linkedin.com/in/andyrooh/">LinkedIn</a>
->>>>>>> 477ee3230261c78cbc4b3815e27cf1edaa4cfa5b
     </footer>
 </>
     )
