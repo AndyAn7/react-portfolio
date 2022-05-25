7a3330d9d133d12e1d5cf4a41c30d14b8c3e5593
Andrew An <andyan7@gmail.com>
2022-05-25 04:14:58 -0700

    real react

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,16 +1,56 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import './index.scss';
 import InstaAndy from '../../assets/images/insta.webp';
 import AndyFace from '../../assets/images/face.webp';
 import LinkyPink from '../../assets/images/linkd.webp';
+import AuthIMG from '../../assets/images/profilePic.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faHome,
+    faBeer,
+    faUser,
+    faUserAlt,
+} from '@fortawesome/free-solid-svg-icons';
 
 const Nav = () => (
-    <div className='navbar'>
+    <div className='nav-bar'>
         <Link className='logo' to='/'>
-            <img src={InstaAndy} alt='instagram' />
-            <img src={AndyFace} alt='face' />
-            <img src={LinkyPink} alt='linkd' />
+            <img className='sub-logo' src={AuthIMG} alt='author' />
+            <div className='iconLinks'>
+                <img src={InstaAndy} alt='instagram' />
+                <img src={AndyFace} alt='face' />
+                <img src={LinkyPink} alt='linkd' />
+            </div>
         </Link>
+
+        <nav>
+            <NavLink exact='true' activeClassName='active' to='/'>
+                <FontAwesomeIcon icon={faHome} />
+            </NavLink>
+            
+            <NavLink exact='true' activeClassName='active' className='about-link' to='/about'>
+                <FontAwesomeIcon icon={faBeer} />
+            </NavLink>
+            
+            <NavLink exact='true' activeClassName='active' to='/project'>
+                <FontAwesomeIcon icon={faUser} />
+            </NavLink>
+            
+            <NavLink exact='true' activeClassName='active' to='/about'>
+                <FontAwesomeIcon icon={faUserAlt} />
+            </NavLink>
+        </nav>
+        
+        <ul>
+            <li>
+                <a 
+                target='_blank'  
+                rel='noreferrer' 
+                href='https://www.instagram.com/ghengisan/'>
+                <img src={InstaAndy} alt='instagram' />
+                </a>
+            </li>
+        </ul>
     </div>
 )
 
