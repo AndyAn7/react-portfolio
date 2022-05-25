68dfc12320283e2e468d02bc2c3aada7a17725c2
Andrew An <andyan7@gmail.com>
2022-05-25 02:14:34 -0700

    new react

--- src/components/Links/index.js
+++ src/components/Links/index.js
@@ -1,19 +1,15 @@
 import './index.scss';
-// import Link from '../About/index.js';
-
 
 const Links = () => {
     return (
         <>
             <nav className="main">
                 <ul>
-                    {/* <Link to="../About">About</Link> */}
-                    {/* <li><Link to="../About/index.js">About</Link></li> */}
                     <li><a href="About">About</a></li>
-                    <li><a href="Github">GitHub</a></li>
-                    <li><a href="LinkedIn">LinkedIn</a></li>
-                    <li><a href="Resume">Resume</a></li>
-                    <li><a href="Contact">Contact</a></li>
+                    <li><a href="https://github.com/AndyAn7">GitHub</a></li>
+                    <li><a href="https://www.linkedin.com/in/andyrooh/">LinkedIn</a></li>
+                    <li><a href="https://docs.google.com/document/d/1T_nu1e2clPNrkOOHCq9DoOZ30ZFxOZcLNMuRUtM9EHo/edit?usp=sharing">Resume</a></li>
+                    <li><a href="mailto: andyan7@gmail.com">Contact</a></li>
                 </ul>
             </nav>
         </>
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,23 +1,35 @@
+import { Link } from 'react-router-dom';
 import './index.scss';
+import InstaAndy from '../../assets/images/insta.webp';
+import AndyFace from '../../assets/images/face.webp';
+import LinkyPink from '../../assets/images/linkd.webp';
 
-const Nav = () => {
-    return (   
-    <>
-        <nav className='navList'>
-            <ul>
-                <li><a href="#aboutModal">About</a></li>
-                
-                <li><a href="#Github">GitHub</a></li>
-                
-                <li><a href="#LinkedIn">LinkedIn</a></li>
-                
-                <li><a href="#Resume">Resume</a></li>
-                
-                <li><a href="#Contact">Contact</a></li>
-            </ul>
-        </nav>
-    </>
-    )
-}
+const Nav = () => (
+    <div className='navbar'>
+        <Link className='logo' to='/'>
+            <img src={InstaAndy} alt='instagram' />
+            <img src={AndyFace} alt='face' />
+            <img src={LinkyPink} alt='linkd' />
+        </Link>
+    </div>
+)
 
 export default Nav;
+ 
+ 
+    // <>
+    //     <nav className='navList'>
+    //         <ul>
+    //             <li><a href="#aboutModal">About</a></li>
+                
+    //             <li><a href="#Github">GitHub</a></li>
+                
+    //             <li><a href="#LinkedIn">LinkedIn</a></li>
+                
+    //             <li><a href="#Resume">Resume</a></li>
+                
+    //             <li><a href="#Contact">Contact</a></li>
+    //         </ul>
+    //     </nav>
+    // </>
+
