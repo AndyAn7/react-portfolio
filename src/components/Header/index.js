9fc7795a981734e0fe6f47dbba5ae9e321e3ffc6
Andrew An <andyan7@gmail.com>
2022-05-26 06:36:53 -0700

    nav icons

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 import './index.scss';
+import { NavLink } from 'react-router-dom';
+import Nav from '../Nav';
 import jhip from '../../assets/images/집.png';
-import { FontAwesomeIcon } from 
-'@fortawesome/react-fontawesome';
+import eel from '../../assets/images/projex.png';
+import about from '../../assets/images/about.png';
+import contact from '../../assets/images/contact.png';
 import { faGithub, faLinkedin, faReact } from 
 '@fortawesome/free-brands-svg-icons';
-import Nav from '../Nav';
+
 
 
 const Header = () => (
@@ -15,19 +17,16 @@ const Header = () => (
         <Nav />
             <nav className='navLinks'>
 
-                <NavLink to='../Home' className='homeLink'><a href='../../assets/images/집.png' className='jhip'><img ClassName='jhip' src={jhip} alt=''/></a>
+                <NavLink to='../Home' className='home'><a href='../../assets/images/집.png' className='jhip'><img ClassName='jhip' src={jhip} alt=''/></a>
                 </NavLink>
 
-                <NavLink exact='true' ClassName='active' to='../Project'>
-                    <FontAwesomeIcon icon='' />
+                <NavLink to='../Project' className='projex'><a href='../../assets/images/projex.png' className='eel'><img ClassName='eel' src={eel} alt=''/></a>
                 </NavLink>
 
-                <NavLink exact='true' ClassName='active' to='../About'>
-                    <FontAwesomeIcon icon='' />
+                <NavLink to='../About' className='about'><a href='../../assets/images/about.png' className='about'><img ClassName='about' src={about} alt=''/></a>
                 </NavLink>
 
-                <NavLink exact='true' ClassName='active' to='../Contact'>
-                    <FontAwesomeIcon icon='' />
+                <NavLink to='../Contact' className='contact'><a href='../../assets/images/contact.png' className='contact'><img ClassName='contact' src={contact} alt=''/></a>
                 </NavLink>
 
             </nav>
