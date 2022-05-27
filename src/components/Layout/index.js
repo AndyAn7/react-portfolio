aad067241c4d026b6aab9548a076600072ee8947
Andrew An <andyan7@gmail.com>
2022-05-26 17:49:01 -0700

    revert to prior commit

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import './index.scss';
 import { Outlet } from 'react-router-dom';
-import { Header, Home, Footer } from '../../components';
+import Header from '../Header';
+import Home from '../Home';
+import Footer from '../Footer';
+
 
 const Layout = () => {
     return (
