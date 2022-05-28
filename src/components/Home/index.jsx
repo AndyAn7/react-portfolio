08f926196a011e15e9eacb2bcf4a46c410b1ed55
Andrew An <andyan7@gmail.com>
2022-05-27 20:50:17 -0700

    attempt to return to previous repo

---
+++ src/components/Home/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './index.scss';
+import 'animate.css';
+
+const Home = () => {
+ return(
+     <>
+
+
+     
+     </>
+ )
+    
+}
+
+export default Home;
---
+++ src/components/Layout/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './index.scss';
+import { Outlet } from 'react-router-dom';
+import Header from '../Header';
+import Home from '../Home';
+import Footer from '../Footer';
+
+
+const Layout = () => {
+    return (
+        <>
+            <Header />
+                <Home />      
+                    <Outlet />    
+            <Footer />
+        </>
+    )
+}
+
+export default Layout;
