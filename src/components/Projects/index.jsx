608e36a11c7d961eea39f217300aaee592ec1b3a
Andrew An <andyan7@gmail.com>
2022-05-30 12:33:46 -0700

    devs

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,20 +1,31 @@
 import React from 'react';
 import './index.sass';
 import Auth from '../Auth';
+import Project from '../Project';
+const jsonProjects = require('../../assets/projects.json')
 
 const Projects = () => {
   return (
     <>
-        <div className='authStyle'>
+      <div className='authStyle'>
 
-          <Auth />
+        <Auth />
 
-          <div className='showcase'>
-            <h1>Projects</h1>
-          </div>
+        <div className='showcase'>
+          <h1>Projects</h1>
+        </div>
 
+      </div>
+
+      <div className="project-page">
+
+        <div className="projects-container">
+          {jsonProjects.map((project) => (
+            <Project key={project.name} project={project} />
+          ))}
         </div>
         
+      </div>
     </>
   )
 }
