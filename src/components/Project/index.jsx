5a28cb7c41b110f657d5cc1f88321338e9f7c2d5
Andrew An <andyan7@gmail.com>
2022-05-30 11:19:14 -0700

    overhaul of devs, update react v18.1, node-sass

--- src/components/Project/index.jsx
+++ src/components/Project/index.jsx
@@ -1,10 +1,49 @@
 import React from 'react';  
 import './index.sass';
 
-const Project = () => {
+const Project = (props) => {
     return(
         <>
-            
+            <div className="card">
+                <div className="card-header">
+                    <h1 className="card-title">{props.project.name}
+                    </h1>
+                    <img
+                    className="card-image"
+                    src={props.project.image}
+                    alt={props.project.name}
+                    />
+                </div>
+
+                <p className="card-text">{props.project.description}
+                </p>
+                <div className="card-links">
+                    <a
+                    href={props.project.github_link}
+                    rel="noreferrer"
+                    target="_blank"
+                    className="card-link"
+                    >
+                    GitHub
+                    </a>
+                    <a
+                    href={props.project.deployed_link}
+                    rel="noreferrer"
+                    target="_blank"
+                    className="card-link"
+                    >   
+                    Deployment
+                    </a>
+                    <a
+                    href={props.project.tutorial}
+                    rel="noreferrer"
+                    target="_blank"
+                    className="card-link"
+                    >   
+                    Tutorial
+                    </a>
+                </div>
+            </div>
         </>
     )
 }
